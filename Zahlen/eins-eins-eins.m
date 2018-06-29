0e33bb8d48eff1fb5033feb24d755d9b54d4b65b
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-06-29 23:34:36 +0200

    Aufgaben in andere Themengebiete verschoben

---
+++ Zahlen/eins-eins-eins.m
@@ -0,0 +1,15 @@
+clear all;
+
+printf("\\begin{exercise}\n");
+printf("      {ID-1c6a6f41857cbda57fd4762f89a75e24a325b27c}\n");
+printf("      {Eins-Eins-Eins}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Gegeben sei eine Primzahl $p>2$. Zeige, dass eine der Zahlen\n");
+printf("    1, 11, 111, 1111, 11111, \\ldots{} durch $p$ teilbar ist.\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
