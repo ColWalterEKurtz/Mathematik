acf2542929b6427d40d2d969cb58a1b6129051cf
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-06-30 16:02:51 +0200

    aufgabe geloest und verschoben

---
+++ LineareGleichungen/jodloesung.m
@@ -0,0 +1,24 @@
+clear all;
+
+printf("\\begin{exercise}\n");
+printf("      {ID-d7970272ce1b80adf244acbf13201b476eef14b7}\n");
+printf("      {Jodlösung}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Eine Lösung von \\pc{16} Jod in Alkohol wiegt \\sig{735}.\n");
+printf("    Es soll eine \\pc{10}-ige Jodlösung hergestellt werden.\n");
+printf("    Wie viel Gramm Alkohol muss man hinzufügen?\n");
+printf("  \\fi\n");
+printf("  \\ifoutline\\outline\n");
+printf("    Die absolute Menge des Jod bleibt in beiden Lösungen gleich\\ldots\n");
+printf("  \\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Die Variable $x$ steht für die Menge Alkohol in Gramm, die man hinzufügen\n");
+printf("    muss, um den Jodanteil der Lösung auf \\pc{10} zu senken.\n");
+printf("    \\begin{equation*}\n");
+printf("      \\num{0.16}\\cdot 735=\\num{0.1}\\cdot(735+x)\n");
+printf("      \\quad\\Rightarrow\\quad\n");
+printf("      x=441\n");
+printf("    \\end{equation*}\n");
+printf("  \\fi\n");
+printf("\\end{exercise}\n");
+
