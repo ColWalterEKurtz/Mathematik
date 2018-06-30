c5f1d227b461e8a5a43a80fb0d45fd3aa6fee9a6
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-06-30 23:51:36 +0200

    neue aufgabe geloest und verschoben

---
+++ LineareGleichungen/museum.m
@@ -0,0 +1,26 @@
+clear all;
+
+printf("\\begin{exercise}\n");
+printf("      {ID-c90563e3a7d7f68a59719a773f24e526da443683}\n");
+printf("      {Museum}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Die Eintrittskarte in ein Museum kostete \\eur{1.50}. Nachdem der Eintrittspreis\n");
+printf("    gesenkt wurde, wuchsen die Besucherzahlen um \\pc{50} und die Einnahmen um \\pc{25}.\n");
+printf("    Um wie viel Cent pro Karte wurde der Preis verringert?\n");
+printf("  \\fi\n");
+printf("  \\ifoutline\\outline\n");
+printf("    Verwende die Variable $x$ für den neuen Preis in Euro, und die Variable $n$ für\n");
+printf("    die Anzahl der Besucher\\ldots\n");
+printf("  \\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Wenn die Variable $x$ für den neuen Preis in Euro steht und die Variable $n$ für\n");
+printf("    die Anzahl der Besucher, dann gilt:\n");
+printf("    \\begin{equation*}\n");
+printf("      (\\num{1.5}\\cdot n)\\cdot x=\\num{1.25}\\cdot(n\\cdot\\eur{1.50})\n");
+printf("      \\quad\\Rightarrow\\quad\n");
+printf("      x=\\num{1.25}\\,\\text{\\officialeuro}\n");
+printf("    \\end{equation*}\n");
+printf("    Also wurde der Preis einer Eintrittskarte um \\eur{0.25} gesenkt.\n");
+printf("  \\fi\n");
+printf("\\end{exercise}\n");
+
