89c729dcca6cfc6f964f2525b428eea68fe6a0c2
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-06-30 15:39:17 +0200

    aufgabe geloest und verschoben

---
+++ Denksport/familie.m
@@ -0,0 +1,33 @@
+clear all;
+
+printf("\\begin{exercise}\n");
+printf("      {ID-3b02cda2482a735aa27ce278c83629ecbcfec0af}\n");
+printf("      {Familie}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Vater, Mutter, Tochter und Sohn sind zusammen 73 Jahre alt.\n");
+printf("    Der Vater ist drei Jahre älter als die Mutter, die Tochter\n");
+printf("    ist 2 Jahre älter als der Sohn. Vor vier Jahren waren\n");
+printf("    alle Familienmitglieder zusammen 58 Jahre alt.\n");
+printf("    Wie alt sind die Familienmitglieder heute?\n");
+printf("  \\fi\n");
+printf("  \\ifoutline\\outline\n");
+printf("    Vielleicht waren vor vier Jahren noch nicht alle Familienmitglieder geboren\\ldots\n");
+printf("  \\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Wenn alle vie Familienmitglieder vor vier Jahren zusammen\n");
+printf("    58 Jahre alt gewesen sind, dann müssten sie heute zusammen\n");
+printf("    $58+16=74$ Jahre alt sein. Aber sie sind zusammen ja nur\n");
+printf("    73 Jahre alt. Vor vier Jahren kann also das jüngste\n");
+printf("    Familienmitglied (der Sohn) noch nicht mitgezählt worden\n");
+printf("    sein, d.\\,h. er war noch gar nicht geboren.\n");
+printf("    \\begin{equation*}\n");
+printf("      (v-4)+(m-4)+(t-4)=58\\quad\\Rightarrow\\quad v+m+t=70\\quad\\Rightarrow\\quad s=3\n");
+printf("    \\end{equation*}\n");
+printf("    Der Sohn ist heute also 3 Jahre alt und seine Schwester folglich 5.\n");
+printf("    \\begin{equation*}\n");
+printf("      (m+3)+m+5=70\\quad\\Rightarrow\\quad m=\\frac{70-5-3}{2}\\quad\\Rightarrow\\quad m=31\n");
+printf("    \\end{equation*}\n");
+printf("    Damit ist die Mutter heute 31 Jahre alt und der Vater 34.\n");
+printf("  \\fi\n");
+printf("\\end{exercise}\n");
+
