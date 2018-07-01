d6f8634e25a6fb5bc14c2951b1168091f9decd37
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-07-01 15:28:33 +0200

    weitere aufgaben mit octave berechnet

--- Pythagoras/klappleiter.m
+++ Pythagoras/klappleiter.m
@@ -1,15 +1,24 @@
 clear all;
 
+l = 3;                   % Leiter
+b = 1.2;                 % Boden
+h = sqrt(l^2 - (b/2)^2); % Hoehe
+
+ins_l = sprintf("%d",   l);
+ins_b = sprintf("%.1f", b);
+ins_h = sprintf("%.2f", h);
+
 printf("\\begin{exercise}\n");
 printf("      {ID-4a108a2283166ccdeb383049a22c13cc1eed2d39}\n");
 printf("      {Klappleiter}\n");
 printf("  \\ifproblem\\problem\n");
-printf("    Eine Klappleiter hat eine Länge von \\simeter{3}. Sie steht am Boden\n");
-printf("    \\simeter{1.2} auseinander. Welche maximale Höhe $h$ in m ist möglich?\n");
+printf("    Eine Klappleiter hat eine Länge von \\simeter{%s}. Sie steht am Boden\n", ins_l);
+printf("    \\simeter{%s} auseinander. Welche maximale Höhe $h$ in m ist möglich?\n", ins_b);
 printf("  \\fi\n");
 printf("  %%\\ifoutline\\outline\n");
 printf("  %%\\fi\n");
-printf("  %%\\ifoutcome\\outcome\n");
-printf("  %%\\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Die Leiter erreicht eine maximale Höhe von \\simeter{%s}.\n", ins_h);
+printf("  \\fi\n");
 printf("\\end{exercise}\n");
 
