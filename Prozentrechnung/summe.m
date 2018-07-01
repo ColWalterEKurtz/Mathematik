a1d8ec92efce35136a07e1b31a9421839a1f8205
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-07-02 00:21:11 +0200

    aufgabe geloest und verschoben

---
+++ Prozentrechnung/summe.m
@@ -0,0 +1,23 @@
+clear all;
+
+printf("\\begin{exercise}\n");
+printf("      {ID-812972e6c6b1b8e11b6ad6d09d94a0a8430c6d93}\n");
+printf("      {Summe}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    In einer Summe ist der erste Summand $\\frac{5}{12}$ des zweiten Summanden.\n");
+printf("    Wie viel Prozent der Summe entfällt auf den kleineren Summanden?\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("  \\begin{equation*}\n");
+printf("    p\\,\\text{\\%%}\n");
+printf("    =\\frac{a}{a+b}\n");
+printf("    =\\frac{\\frac{5}{12}b}{\\frac{5}{12}b+b}\n");
+printf("    =\\frac{\\frac{5}{12}b}{\\left(\\frac{5}{12}+1\\right)\\cdot b}\n");
+printf("    =\\frac{5}{17}\n");
+printf("    \\approx\\num{29.4}\\,\\text{\\%%}\n");
+printf("  \\end{equation*}\n");
+printf("  \\fi\n");
+printf("\\end{exercise}\n");
+
