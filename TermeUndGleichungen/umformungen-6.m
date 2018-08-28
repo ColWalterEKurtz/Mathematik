26237f32f02f9b5530f66305126a79f9963cc7b9
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-08-28 19:33:19 +0200

    aufgaben verschoben

---
+++ TermeUndGleichungen/umformungen-6.m
@@ -0,0 +1,28 @@
+clear all;
+
+printf("\\begin{exercise}\n");
+printf("      {ID-5865f69dad63d1e6b9b6913382f6f23eaec96dc0}\n");
+printf("      {Umformungen 6}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    \\newcommand{\\gap}{\\;\\;}%%\n");
+printf("    Löse die Klammern auf, und fasse so weit wie möglich zusammen:\n");
+printf("    \\begin{align*}\n");
+printf("      \\text{a)}\\gap & (p+q)^2+(p-q)^2 &\n");
+printf("      \\text{e)}\\gap & (2a+1)^2-(a-3)^2\n");
+printf("      \\\\\n");
+printf("      \\text{b)}\\gap & (3p+2q)^2-(2p-3q)^2 &\n");
+printf("      \\text{f)}\\gap & (c+2d)(c-2d)+(c-d)(2c+d)\n");
+printf("      \\\\\n");
+printf("      \\text{c)}\\gap & (a+3b)^2+(3a+b)(3a-b) &\n");
+printf("      \\text{g)}\\gap & (3x+2)(1-x)-(x-4)^2\n");
+printf("      \\\\\n");
+printf("      \\text{d)}\\gap & (5x+z)(5x-z)-(2x-5z)^2 &\n");
+printf("      \\text{h)}\\gap & 5(y-2)^2-3(y+2)^2\n");
+printf("    \\end{align*}\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
---
+++ TermeUndGleichungen/umformungen-7.m
@@ -0,0 +1,28 @@
+clear all;
+
+printf("\\begin{exercise}\n");
+printf("      {ID-d3e260844f8b01fa5b0d2fc0a2b0bc9b46069915}\n");
+printf("      {Umformungen 7}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    \\newcommand{\\gap}{\\;\\;}%%\n");
+printf("    Löse die Klammern auf, und fasse so weit wie möglich zusammen:\n");
+printf("    \\begin{align*}\n");
+printf("      \\text{a)}\\gap & (2a+b)^3 &\n");
+printf("      \\text{e)}\\gap & (3x-2)^4\n");
+printf("      \\\\\n");
+printf("      \\text{b)}\\gap & (a-3b)^3 &\n");
+printf("      \\text{f)}\\gap & (2m+1)^5\n");
+printf("      \\\\\n");
+printf("      \\text{c)}\\gap & (5-y)^3 &\n");
+printf("      \\text{g)}\\gap & (x+2)^3+(x-2)^3\n");
+printf("      \\\\\n");
+printf("      \\text{d)}\\gap & (x+2)^4 &\n");
+printf("      \\text{h)}\\gap & (z+1)^4-(z-1)^4\n");
+printf("    \\end{align*}\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
