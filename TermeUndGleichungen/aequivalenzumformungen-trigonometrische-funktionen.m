65e60fae9fe589ca0c1a5fc684b7f1d0882b4d2b
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-08-29 20:54:04 +0200

    aequivalenzumformungen fuer trigonometrische funktionen hinzugefuegt

---
+++ TermeUndGleichungen/aequivalenzumformungen-trigonometrische-funktionen.m
@@ -0,0 +1,33 @@
+clear all;
+
+printf("\\begin{exercise}\n");
+printf("      {ID-ed1f5163e03129a5bc890e64155e5d0facb02d2c}\n");
+printf("      {Äquivalenzumformungen: Trigonometrische Funktionen}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    \\newcommand{\\exnum}[1]{\\text{\\rule[-2.5ex]{0pt}{6ex}#1}}\n");
+printf("    Löse folgende Gleichungen nach der angegebenen Variable auf:\n");
+printf("    \\allowdisplaybreaks\n");
+printf("    \\begin{alignat*}{4}\n");
+printf("      \\exnum{a)}&\\quad &          c^{2}&=a^{2}+b^{2}-2ab\\cos\\gamma                                                  & \\quad&\\quad & \\quad&\\text{nach $\\gamma$}     \\\\\n");
+printf("      \\exnum{b)}&\\quad & \\frac{abc}{2F}&=\\frac{a}{\\sin\\alpha}                                                       & \\quad&\\quad & \\quad&\\text{nach $\\alpha$}     \\\\\n");
+printf("      \\exnum{c)}&\\quad &           \\rho&=r\\left(\\cos\\alpha+\\cos\\beta+\\cos\\gamma-1\\right)                            & \\quad&\\quad & \\quad&\\text{nach $\\beta$}      \\\\\n");
+printf("      \\exnum{d)}&\\quad &              s&=4r\\cos\\frac{\\alpha}{2}\\cos\\frac{\\beta}{2}\\cos\\frac{\\gamma}{2}              & \\quad&\\quad & \\quad&\\text{nach $\\alpha$}     \\\\\n");
+printf("      \\exnum{e)}&\\quad &              w_{\\alpha}&=\\frac{2F}{a\\cos\\frac{\\beta-\\gamma}{2}}                            & \\quad&\\quad & \\quad&\\text{nach $\\gamma$}     \\\\\n");
+printf("      \\exnum{f)}&\\quad &              s&=\\frac{v_{0}^{2}\\cdot\\sin2\\alpha}{g}                                        & \\quad&\\quad & \\quad&\\text{nach $\\alpha$}     \\\\\n");
+printf("      \\exnum{g)}&\\quad &              h&=\\frac{v_{0}^{2}\\cdot\\sin^{2}\\alpha}{2g}                                    & \\quad&\\quad & \\quad&\\text{nach $\\alpha$}     \\\\\n");
+printf("      \\exnum{h)}&\\quad &              n&=\\frac{\\sin\\alpha_{p}}{\\sin(90^{\\circ}-\\alpha_{p})}                         & \\quad&\\quad & \\quad&\\text{nach $\\alpha_{p}$} \\\\\n");
+printf("      \\exnum{i)}&\\quad &              a&=\\frac{\\delta\\sin(\\alpha-\\beta)}{\\cos\\beta}                                 & \\quad&\\quad & \\quad&\\text{nach $\\beta$}      \\\\\n");
+printf("      \\exnum{j)}&\\quad &        \\Gamma&=\\frac{\\tan\\sigma}{\\tan\\sigma_{0}}                                           & \\quad&\\quad & \\quad&\\text{nach $\\sigma_{0}$} \\\\\n");
+printf("      \\exnum{k)}&\\quad &        \\sigma&=\\arctan\\frac{y}{a}                                                          & \\quad&\\quad & \\quad&\\text{nach $a$}          \\\\\n");
+printf("      \\exnum{l)}&\\quad &    \\sin\\alpha&=\\cos\\alpha                                                                  & \\quad&\\quad & \\quad&\\text{nach $\\alpha$}     \\\\\n");
+printf("      \\exnum{m)}&\\quad &    \\cos\\alpha&=\\sin(\\alpha)\\cdot\\frac{\\cos^{2}(\\pi\\alpha)}{\\cot^{2}(\\pi\\alpha)}\n");
+printf("                                       -\\cos(\\alpha+90^{\\circ})\\cdot\\frac{\\sin^{2}(\\pi\\alpha)}{\\tan^{2}(\\pi\\alpha)} & \\quad&\\quad & \\quad&\\text{nach $\\alpha$}     \\\\\n");
+printf("      \\exnum{n)}&\\quad &              y&=x\\tan\\alpha-\\frac{g}{2{v_{0}}^{2}\\cos^{2}\\alpha}x^{2}                      & \\quad&\\quad & \\quad&\\text{nach $\\alpha$}\n");
+printf("    \\end{alignat*}\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
