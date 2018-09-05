e9df251f3fc1b553c75ff6009c771509eab9441c
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-09-05 10:39:05 +0200

    aufgabe 'snowboardhelme' berechnet

--- QuadratischeGleichungen/snowboardhelme.m
+++ QuadratischeGleichungen/snowboardhelme.m
@@ -1,5 +1,18 @@
 clear all;
 
+addpath 'Octave/';
+
+a  = -1;
+b  = 70;
+c  = -1000;
+xs = -b / (2 * a);
+ys = a * xs^2 + b * xs + c;
+
+ins_term = polystr([a b c], 'x');
+ins_xs   = sprintf("%g", xs);
+ins_ys   = sprintf("%g", ys);
+
+
 printf("\\begin{exercise}\n");
 printf("      {ID-ef133af43d4fe887ec6f1ed648d4669454cae0dd}\n");
 printf("      {Snowboardhelme}\n");
@@ -17,9 +30,19 @@ printf("            anbieten?\n");
 printf("      \\item Wie groß wäre dann der tägliche Gewinn?\n");
 printf("    \\end{enumerate}\n");
 printf("  \\fi\n");
-printf("  %%\\ifoutline\\outline\n");
-printf("  %%\\fi\n");
-printf("  %%\\ifoutcome\\outcome\n");
-printf("  %%\\fi\n");
+printf("  \\ifoutline\\outline\n");
+printf("    \\begin{enumerate}[a)]\n");
+printf("      \\item Gesucht ist die $x$-Koordinate des Scheitelpunkts.\n");
+printf("      \\item Gesucht ist die $y$-Koordinate des Scheitelpunkts.\n");
+printf("    \\end{enumerate}\n");
+printf("  \\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    \\begin{enumerate}[a)]\n");
+printf("      \\item Das Unternehmen sollte die Snowboardhelme für \\eur{%s} pro Stück verkaufen.\n", ins_xs);
+printf("      \\item Der tägliche Gewinn läge dann bei \\eur{%s}.\n", ins_ys);
+printf("    \\end{enumerate}\n");
+printf("  \\fi\n");
 printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
 
