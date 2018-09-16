6a0f3841a5c212129d5d31fbb196608f636dc699
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-09-16 03:13:43 +0200

    aufgabe 'von - mehr als - weniger als' hinzugefuegt

---
+++ Prozentrechnung/von-mehr-als-weniger-als.m
@@ -0,0 +1,43 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-797012f1faa18c18c26785bbd21015ab6096071e}\n");
+printf("      {Von -- mehr als -- weniger als}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Übersetze in einen Term und berechne:\\\\\n");
+printf("    \\begin{minipage}[t]{0.32\\textwidth}\n");
+printf("      \\begin{enumerate}[a)]\n");
+printf("        \\item \\pc{5} von 30\\,kg\n");
+printf("        \\item \\pc{5} mehr als 30\\,kg\n");
+printf("        \\item \\pc{5} weniger als 30\\,kg\n");
+printf("      \\end{enumerate}\n");
+printf("    \\end{minipage}%%\n");
+printf("    \\hfill\n");
+printf("    \\begin{minipage}[t]{0.32\\textwidth}\n");
+printf("      \\begin{enumerate}[a)]\n");
+printf("        \\setcounter{enumi}{3}\n");
+printf("        \\item \\pc{75} von 6\\,m\n");
+printf("        \\item \\pc{75} mehr als 6\\,m\n");
+printf("        \\item \\pc{75} weniger als 6\\,m\n");
+printf("      \\end{enumerate}\n");
+printf("    \\end{minipage}%%\n");
+printf("    \\hfill\n");
+printf("    \\begin{minipage}[t]{0.32\\textwidth}\n");
+printf("      \\begin{enumerate}[a)]\n");
+printf("        \\setcounter{enumi}{6}\n");
+printf("        \\item \\pc{400} von \\eur{50}\n");
+printf("        \\item \\pc{400} mehr als \\eur{50}\n");
+printf("        \\item \\pc{400} weniger als \\eur{50}\n");
+printf("      \\end{enumerate}\n");
+printf("    \\end{minipage}\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
