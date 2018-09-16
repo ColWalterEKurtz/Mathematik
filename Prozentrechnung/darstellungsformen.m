e0f55a54f2f3b432e43bd98f97cf172540b090c4
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-09-16 03:20:32 +0200

    aufgabe 'darstellungsformen' hinzugefuegt

---
+++ Prozentrechnung/darstellungsformen.m
@@ -0,0 +1,24 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-0d22bc540ce1c9f607acc5c6d917375bb5a91d19}\n");
+printf("      {Darstellungsformen}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Schreibe die Prozentangabe als Bruch und als Dezimalzahl:\n");
+printf("    \\begin{alignat*}{6}\n");
+printf("      \\text{a)}&\\quad & \\text{\\pc{1}}&\\qquad\\qquad  & \\text{e)}&\\quad & \\text{\\pc{75}}&\\qquad\\qquad  & \\text{i)}&\\quad & \\text{\\pc{1.5}}&    \\\\[1ex]\n");
+printf("      \\text{b)}&\\quad & \\text{\\pc{10}}&\\qquad\\qquad & \\text{f)}&\\quad & \\text{\\pc{99}}&\\qquad\\qquad  & \\text{j)}&\\quad & \\text{\\pc{17.125}}& \\\\[1ex]\n");
+printf("      \\text{c)}&\\quad & \\text{\\pc{25}}&\\qquad\\qquad & \\text{g)}&\\quad & \\text{\\pc{100}}&\\qquad\\qquad & \\text{k)}&\\quad & \\text{\\pc{60.25}}&  \\\\[1ex]\n");
+printf("      \\text{d)}&\\quad & \\text{\\pc{50}}&\\qquad\\qquad & \\text{h)}&\\quad & \\text{\\pc{119}}&\\qquad\\qquad & \\text{l)}&\\quad & \\text{\\pc{0.1}}&\n");
+printf("    \\end{alignat*}\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
