b8415ab97593e9ff671fa5277d662e0fcfb2723e
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2018-09-29 00:39:12 +0200

    mehr aufgaben zum thema extremwertprobleme hinzugefuegt

---
+++ Extremwertprobleme/kegel-im-kegel.m
@@ -0,0 +1,21 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-13e19f7300d1d835ccf105e1fb6255b2143a4cd1}\n");
+printf("      {Kegel im Kegel}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Einem Kegel mit dem Radius $R$ und der Höhe $H$ soll ein zweiter Kegel so\n");
+printf("    einbeschrieben werden, dass dessen Spitze im Mittelpunkt der Grundfläche\n");
+printf("    liegt. Mit welchem Radius $r$ und mit welcher Höhe $h$ wird das Volumen des\n");
+printf("    einbeschriebenen Kegels maximal?\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
---
+++ Extremwertprobleme/maximaler-zylinder.m
@@ -0,0 +1,19 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-afefae67c6119502c34c7c0929e4e528fe9ac2da}\n");
+printf("      {Maximaler Zylinder}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Bestimmen Sie den Radius und die Höhe des Zylindes, der unter allen Zylindern\n");
+printf("    mit gleichgroßer Oberfläche das größte Volumen besitzt.\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
