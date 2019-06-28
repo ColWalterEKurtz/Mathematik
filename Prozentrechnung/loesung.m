ba54e36131f39a923fcbd931f15699ddcb03b7fa
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2019-06-28 13:16:30 +0200

    neue aufgabe: prozentrechnung - loesung

---
+++ Prozentrechnung/loesung.m
@@ -0,0 +1,22 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-aad90c4416cee91998d3d1bded42a8c0fcfb3628}\n");
+printf("      {Lösung}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    In einem Behälter befinden sich genau \\sikg{25} einer \\pc{4}-igen wässrigen\n");
+printf("    Lösung, d.\\,h., \\pc{4} dieser Lösung bestehen aus der gelösten Substanz,\n");
+printf("    der Rest besteht aus Wasser.\\par\n");
+printf("    Wieviel Prozent des Wassers sind dieser Lösung zu entziehen, damit eine\n");
+printf("    neue Lösung entsteht, deren Wasseranteil nur noch \\pc{90} beträgt?\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
