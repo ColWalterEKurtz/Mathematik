e1d66d507fb788cc812420ff3aad0b147bb32c6d
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2019-12-29 22:22:13 +0100

    aufgabe hinzugefuegt: golfball; aufgabe einsortiert: schrittlaenge

---
+++ QuadratischeGleichungen/golfball.m
@@ -0,0 +1,106 @@
+clear all;
+
+addpath 'Octave/';
+
+function y = f(a, b, c, x)
+
+  y = a.*x.^2 .+ b.*x .+ c;
+
+endfunction
+
+a = -0.007;
+b =  0.9;
+c =  0.0;
+
+x = [-10:1:140]';
+y = f(a, b, c, x);
+
+cnum = 1;
+cmax = 4;
+
+cnum=1;
+cmax=4;
+printf("\\begin{exercise}\n");
+printf("      {ID-49712269135e7d4b30bf0d643149cc3fa2bb48c2}\n");
+printf("      {Golfball}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Der Flug eines Golfballs kann näherungsweise durch eine Parabel modelliert werden.\n");
+printf("    Der Graph ist in folgendem Koordinatensystem abgebildet.\n");
+printf("    \\begin{center}\n");
+printf("      \\newcommand{\\xmark}[2]{\\draw (#1, 2) -- (#1, -2) node[below]{\\vphantom{\\ensuremath{(}}#2};}%%\n");
+printf("      \\newcommand{\\ymark}[2]{\\draw (2, #1) -- (-2, #1) node[left]{\\vphantom{\\ensuremath{(}}#2};}%%\n");
+printf("      \\begin{tikzpicture}[scale=0.0625]\n");
+printf("        \\draw[line width=0.66pt, ->, >=stealth] (-20, 0) -- (150, 0) node[below] {$x$};;\n");
+printf("        \\xmark{-10}{}\n");
+printf("        \\xmark{10}{10}\n");
+printf("        \\xmark{20}{}\n");
+printf("        \\xmark{30}{}\n");
+printf("        \\xmark{40}{}\n");
+printf("        \\xmark{50}{50}\n");
+printf("        \\xmark{60}{}\n");
+printf("        \\xmark{70}{}\n");
+printf("        \\xmark{80}{}\n");
+printf("        \\xmark{90}{}\n");
+printf("        \\xmark{100}{100}\n");
+printf("        \\xmark{110}{}\n");
+printf("        \\xmark{120}{}\n");
+printf("        \\xmark{130}{}\n");
+printf("        \\xmark{140}{}\n");
+printf("        \\draw[line width=0.66pt, ->, >=stealth] (0, -20) -- (0, 50) node[left] {$y$};\n");
+printf("        \\ymark{-10}{}\n");
+printf("        \\ymark{10}{10}\n");
+printf("        \\ymark{20}{20}\n");
+printf("        \\ymark{30}{30}\n");
+printf("        \\ymark{40}{40}\n");
+printf("        \\begin{scope}\n");
+
+printf("          \\draw plot[smooth] coordinates\n");
+printf("          {\n");
+for i = 1:size(x)
+
+  if (cnum > cmax)
+    printf("\n");
+    cnum = 1;
+  endif
+
+  if (cnum == 1)
+    printf("            ");
+  else
+    printf("  ");
+  endif
+
+  printf("(%7.3f, %7.3f)", x(i), y(i));
+
+  cnum += 1;
+
+endfor
+printf("\n          };\n");
+
+printf("        \\end{scope}\n");
+printf("      \\end{tikzpicture}\n");
+printf("    \\end{center}\n");
+printf("    \\begin{enumerate}[a)]\n");
+printf("      \\item Begründe, ob die Flugbahn durch die Funktionsgleichung\n");
+printf("            \\begin{equation*}\n");
+printf("                y_1=\\num{-0.007}x^2+\\num{0.9}x\n");
+printf("                \\quad,\\quad\n");
+printf("                y_2=\\num{0.007}x^2+\\num{0.9}x\n");
+printf("                \\quad\\text{oder}\\quad\n");
+printf("                y_3=\\num{-0.07}x\n");
+printf("            \\end{equation*}\n");
+printf("            beschrieben werden kann.\n");
+printf("      \\item Gib die maximale Höhe des Golfballs an.\n");
+printf("      \\item Fliegt ein Ball, dessen Flugbahn durch die Punkte\n");
+printf("            $P(0\\mid0)$, $Q(10\\mid\\num{10.3})$ und $R(20\\mid\\num{19.2})$\n");
+printf("            verläuft, höher bzw. weiter als der oben beschriebene Golfball?\n");
+printf("            Begründe deine Antwort.\n");
+printf("    \\end{enumerate}\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
---
+++ Zahlen/schrittlaenge.m
@@ -0,0 +1,25 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-e9db77c9a5bbbfd8e37ea60ee54738c90e3cccfc}\n");
+printf("      {Schrittlänge}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Vater und Sohn messen mit ihren Schritten den Abstand zwischen zwei Bäumen.\n");
+printf("    Zuerst schreitet der Vater die Strecke ab, danach sein Sohn. Als der Sohn\n");
+printf("    losgeht, ist auf dem Boden die Spur des Vaters deutlich zu erkennen.\n");
+printf("    Der Sohn tritt insgesamt 10 Mal genau auf den Fußabdruck seines Vaters --\n");
+printf("    zum Glück auch am Ende der Strecke. Ein Schritt des Vaters ist \\sicm{70}\n");
+printf("    lang, ein Schritt des Sohnes misst \\sicm{56}. Wie weit sind die beiden\n");
+printf("    Bäume voneinander entfernt?\n");
+printf("  \\fi\n");
+printf("  \\ifoutline\\outline\n");
+printf("    Vielleicht hilft das kleinste gemeinsame Vielfache\\ldots\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
