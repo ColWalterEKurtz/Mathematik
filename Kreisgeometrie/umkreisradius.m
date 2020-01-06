e432bc501a4feb1bcf470861407602a39ae8f686
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-01-06 15:59:03 +0100

    reihenfolge der aufgaben geaendert

---
+++ Kreisgeometrie/umkreisradius.m
@@ -0,0 +1,22 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-facf99e6e467cb97bc1fdc9e8ad48b33e9153f66}\n");
+printf("      {Umkreisradius}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    In einem Dreieck $ABC$ sei $u$ die Länge des Umfangs, und $r$ sei die Länge des Umkreisradius.\n");
+printf("    Beweise, dass dann stets die folgende Ungleichung gilt:\n");
+printf("    \\begin{equation*}\n");
+printf("      r>\\frac{u}{6}\n");
+printf("    \\end{equation*}\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
