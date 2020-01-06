0d5e9ed0b6479f744247b271c410e1e534457fcf
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-01-06 09:32:56 +0100

    zwei aufgaben einsortiert

---
+++ Pythagoras/seitenlaenge.m
@@ -0,0 +1,20 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-2ff5f81fbb7bfcfc8e89ade2a20c1f80e355ab92}\n");
+printf("      {Seitenlänge}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Die Diagonale eines Rechtecks ist \\sicm{20} lang.\n");
+printf("    Die Seitenlängen unterscheiden sich um \\sicm{8}.\n");
+printf("    Bestimme die Länge der beiden Seiten.\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
---
+++ QuadratischeGleichungen/kantenlaenge.m
@@ -0,0 +1,20 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-f13c259341ab8a066d860395f49cf30ca59b6fa5}\n");
+printf("      {Kantenlänge}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Das Volumen eines Quaders beträgt $V=\\sicmmm{84}$, die Breite\n");
+printf("    $a=\\sicm{7}$. Die Höhe $c$ soll um \\sicm{1} kürzer sein, als die Länge $b$.\n");
+printf("    Wie lang sind die Kanten $b$ und $c$?\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
