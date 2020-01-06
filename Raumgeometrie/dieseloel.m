238fb5e3a781f9c00e7454886b302b0797d3bbd8
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-01-06 10:23:52 +0100

    aufgaben hinzugefuegt: dieseloel, vesuv

---
+++ Raumgeometrie/dieseloel.m
@@ -0,0 +1,24 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-ea13ecc0acf02bc26fdd115fa1fb94e38bb23f40}\n");
+printf("      {Dieselöl}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Der Durchmesser eines \\simeter{1.40} langen Fasses für Dieselöl\n");
+printf("    beträgt \\sicm{90} (Innenmaße). Das leere Fass wiegt \\sikg{26}.\n");
+printf("    \\sidmmm{1} Dieselöl wiegt \\sikg{0.94}.\n");
+printf("    \\begin{enumerate}[a)]\n");
+printf("      \\item Wie viel wiegt das Dieselölfass, wenn es halb gefüllt ist?\n");
+printf("      \\item Wie viel würde das Fass wiegen, wenn es mit Wasser gefüllt wäre?\n");
+printf("    \\end{enumerate}\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
---
+++ Raumgeometrie/vesuv.m
@@ -0,0 +1,21 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-39928549f59ad89a5da4247068264a43381250c8}\n");
+printf("      {Vesuv}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Der Vesuv ist etwa \\simeter{1280} hoch. Sein Krater hat einen Durchmesser\n");
+printf("    von ca. \\simeter{600}, läuft mehr oder weniger spitz nach unten zu und\n");
+printf("    ist rund \\simeter{200} tief. An seinem Fuß hat der Vesuv einen Umfang\n");
+printf("    von ungefähr \\sikm{13}. Wie viel Erde enthält der Vesuv näherungsweise?\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
