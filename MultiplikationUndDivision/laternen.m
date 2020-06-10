15f98e793ce122db6f8fb066368e02b08706c3cf
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-06-10 22:54:25 +0200

    neues kapitel angefangen: addition und multiplikation

---
+++ MultiplikationUndDivision/laternen.m
@@ -0,0 +1,24 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-5cdd9e1fd079bcbcb6853571087cbe536f94db6c}\n");
+printf("      {Laternen}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    In einer kleinen Stadt stehen auf einer Straße am linken und am rechten\n");
+printf("    Straßenrand insgesamt 47 Laternen.\n");
+printf("    Auf jeder Straßenseite beträgt der Abstand zwischen je zwei benachbarten\n");
+printf("    Laternen \\simeter{35}.\n");
+printf("    Am linken Straßenrand steht je eine Laterne genau am Anfang und am Ende\n");
+printf("    der Straße.\n");
+printf("    Wie lang ist die Straße?\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
