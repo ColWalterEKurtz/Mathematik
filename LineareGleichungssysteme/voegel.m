edafd731f947e0926b7a4c8f696b67bbe9cc30a6
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-06-11 15:08:44 +0200

    neue aufgaben: schotter, voegel

---
+++ LineareGleichungssysteme/voegel.m
@@ -0,0 +1,22 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-0993bc1925a2ffbaf3da35dd3cdd103b1c84913c}\n");
+printf("      {Vögel}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Auf drei Bäumen sitzen insgesamt 56 Vögel. Nachdem vom ersten Baum 7 auf\n");
+printf("    den zweiten und vom zweiten 5 Vögel auf den dritten Baum geflogen waren,\n");
+printf("    saßen nun auf dem zweiten Baum doppelt so viele Vögel wie auf dem ersten\n");
+printf("    und auf dem dritten doppelt so viele Vögel wie auf dem zweiten Baum.\n");
+printf("    Berechne, wie viele Vögel ursprünglich auf jedem der Bäume saßen.\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
---
+++ MultiplikationUndDivision/schotter.m
@@ -0,0 +1,24 @@
+clear all;
+
+addpath 'Octave/';
+
+printf("\\begin{exercise}\n");
+printf("      {ID-0103181cfbeb93a1e37d80d98ef66c350ac29d27}\n");
+printf("      {Schotter}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Zum Transport einer bestimmten Menge Schotter hätte ein LKW\n");
+printf("    mit \\sitn{5} Ladefähigkeit genau 105 vollbeladene Fuhren\n");
+printf("    durchführen müssen. Nach 35 dieser Fuhren wurde er durch\n");
+printf("    einen anderen LKW mit \\sitn{7} Ladefähigkeit abgelöst.\n");
+printf("    Stelle fest, wie viele vollbeladene Fuhren dieser zweite\n");
+printf("    LKW noch durchzuführen hat, um die restliche Schottermenge\n");
+printf("    abzutransportieren.\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
