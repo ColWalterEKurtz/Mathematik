18092519b15bb81be2328a756b1f88140f835be3
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-07-14 21:14:53 +0200

    ergebnis berechnet: klebeband

--- WuerfelUndQuader/klebeband.m
+++ WuerfelUndQuader/klebeband.m
@@ -2,6 +2,20 @@ clear all;
 
 addpath 'Octave/';
 
+laenge = 45;
+breite = 30;
+hoehe  = 25;
+uelapp = 10;
+bandcm = 2 * (laenge + 2 * breite + 3 * hoehe) + uelapp;
+bandm  = bandcm / 100;
+
+ins_laenge = sprintf("%d",   laenge);
+ins_breite = sprintf("%d",   breite);
+ins_hoehe  = sprintf("%d",   hoehe);
+ins_uelapp = sprintf("%d",   uelapp);
+ins_bandcm = sprintf("%d",   bandcm);
+ins_bandm  = sprintf("%.2f", bandm);
+
 printf("\\begin{exercise}\n");
 printf("      {ID-b75ee6a9f0c1b08630cdb754aa7c8602714f4880}\n");
 printf("      {Klebeband}\n");
@@ -27,19 +41,21 @@ printf("        \\end{scope}\n");
 printf("      \\end{tikzpicture}%%\n");
 printf("    \\end{minipage}\\hfill\n");
 printf("    \\begin{minipage}{0.65\\linewidth}\n");
-printf("      Das in der Abbildung abgebildete Paket ist von links nach rechts \\sicm{45}\n");
-printf("      lang, von vorn nach hinten \\sicm{30} breit, und es ist \\sicm{25} hoch.\n");
+printf("      Das in der Abbildung abgebildete Paket ist von links nach rechts \\sicm{%s}\n", ins_laenge);
+printf("      lang, von vorn nach hinten \\sicm{%s} breit, und es ist \\sicm{%s} hoch.\n", ins_breite, ins_hoehe);
 printf("      Es soll in der dargestellten Weise (gestrichelte Linie) mit Klebeband\n");
 printf("      verklebt werden. Für das Überlappen von End- und Anfangsstücken sind\n");
-printf("      zusätzlich insgesamt \\sicm{10} Klebeband vorgesehen.\\par\n");
+printf("      zusätzlich insgesamt \\sicm{%s} Klebeband vorgesehen.\\par\n", ins_uelapp);
 printf("      Wie viel Zentimeter Klebeband werden demnach insgesamt gebraucht?\n");
 printf("      Wie viel Meter sind das?\n");
 printf("    \\end{minipage}\n");
 printf("  \\fi\n");
 printf("  %%\\ifoutline\\outline\n");
 printf("  %%\\fi\n");
-printf("  %%\\ifoutcome\\outcome\n");
-printf("  %%\\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Zum Verkleben des Paketes werden insgesamt \\sicm{%s} Klebeband benötigt.\n", ins_bandcm);
+printf("    Das sind \\simeter{%s}.\n", ins_bandm);
+printf("  \\fi\n");
 printf("\\end{exercise}\n");
 
 rmpath 'Octave/';
