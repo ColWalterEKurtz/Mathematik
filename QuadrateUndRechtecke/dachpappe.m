32fae25d2eedca13b18e3ff162b0e9a85a903993
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-07-14 16:52:25 +0200

    ergebnis berechnet: dachpappe

--- QuadrateUndRechtecke/dachpappe.m
+++ QuadrateUndRechtecke/dachpappe.m
@@ -1,16 +1,29 @@
 clear all;
 
+addpath 'Octave/';
+
+laenge  = 11;
+breite  = 3;
+qmpreis = 7.3;
+kosten  = laenge * breite * qmpreis;
+
+ins_laenge  = sprintf("%d",   laenge);
+ins_breite  = sprintf("%d",   breite);
+ins_qmpreis = sprintf("%.2f", qmpreis);
+ins_kosten  = sprintf("%.2f", kosten);
+
 printf("\\begin{exercise}\n");
 printf("      {ID-474be168452848bf3c3c2059e2d6da9e9a1c9d4e}\n");
 printf("      {Dachpappe}\n");
 printf("  \\ifproblem\\problem\n");
-printf("    Das Dach eines Schuppens ist \\simeter{11} lang und \\simeter{3} breit; es soll\n");
-printf("    mit Dachpappe belegt werden, von der jeder Quadratmeter \\eur{7.30} kostet.\n");
+printf("    Das Dach eines Schuppens ist \\simeter{%s} lang und \\simeter{%s} breit; es soll\n", ins_laenge, ins_breite);
+printf("    mit Dachpappe belegt werden, von der jeder Quadratmeter \\eur{%s} kostet.\n", ins_qmpreis);
 printf("    Wie viel kostet der Dachpappenbelag?\n");
 printf("  \\fi\n");
 printf("  %%\\ifoutline\\outline\n");
 printf("  %%\\fi\n");
-printf("  %%\\ifoutcome\\outcome\n");
-printf("  %%\\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Insgesamt muss man für die Dachpappe \\eur{%s} bezahlen.\n", ins_kosten);
+printf("  \\fi\n");
 printf("\\end{exercise}\n");
 
