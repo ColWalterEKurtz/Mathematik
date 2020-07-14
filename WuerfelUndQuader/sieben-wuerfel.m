6d9a4cc5e7b12b28df5f67490837789940a68e02
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-07-14 20:51:35 +0200

    ergebnis berechnet: sieben wuerfel

--- WuerfelUndQuader/sieben-wuerfel.m
+++ WuerfelUndQuader/sieben-wuerfel.m
@@ -1,11 +1,23 @@
 clear all;
 
+addpath 'Octave/';
+
+oberflaeche = 1920;
+quadrat     = oberflaeche / ((3 + 5 + 7) * 2);
+seite       = sqrt(quadrat);
+volumen     = 7 * seite^3;
+
+ins_oberflaeche = sprintf("%d", oberflaeche);
+ins_quadrat     = sprintf("%d", quadrat);
+ins_seite       = sprintf("%d", seite);
+ins_volumen     = sprintf("%d", volumen);
+
 printf("\\begin{exercise}\n");
 printf("      {ID-a42903853dfdea65c0592ab2d4f701ca97723472}\n");
 printf("      {Sieben Würfel}\n");
 printf("  \\ifproblem\\problem\n");
 printf("    Der abgebildete Körper besteht aus sieben gleich großen Würfeln und\n");
-printf("    besitzt eine Oberfläche von 1\\,920\\,cm${}^{2}$. Wie groß ist sein Volumen?\n");
+printf("    besitzt eine Oberfläche von \\sicmm{%s}. Wie groß ist sein Volumen?\n", ins_oberflaeche);
 printf("    \\begin{center}\n");
 printf("      \\begin{tikzpicture}[scale=1.5]\n");
 printf("        %% Umriss\n");
@@ -37,9 +49,11 @@ printf("        \\draw [line width=0.5pt] (0, 1, 1) -- (0, 1, 0);\n");
 printf("      \\end{tikzpicture}\n");
 printf("    \\end{center}\n");
 printf("  \\fi\n");
-printf("  %%\\ifoutline\\outline\n");
-printf("  %%\\fi\n");
-printf("  %%\\ifoutcome\\outcome\n");
-printf("  %%\\fi\n");
+printf("  \\ifoutline\\outline\n");
+printf("    Die Oberfläche besteht aus $(3+5+7)\\cdot2=%d$ Quadraten.\n", (3 + 5 + 7) * 2);
+printf("  \\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Das Volumen dieses Körpers beträgt \\sicmmm{%s}.\n", ins_volumen);
+printf("  \\fi\n");
 printf("\\end{exercise}\n");
 
