559bed1eac07178c88b1c6d6de426865b9628aba
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-07-14 16:41:41 +0200

    ergebnis berechnet: farbe

--- QuadrateUndRechtecke/farbe.m
+++ QuadrateUndRechtecke/farbe.m
@@ -1,15 +1,28 @@
 clear all;
 
+addpath 'Octave/';
+
+laenge  = 5;
+breite  = 4;
+qmpreis = 14;
+kosten  = laenge * breite * qmpreis;
+
+ins_laenge  = sprintf("%d", laenge);
+ins_breite  = sprintf("%d", breite);
+ins_qmpreis = sprintf("%d", qmpreis);
+ins_kosten  = sprintf("%d", kosten);
+
 printf("\\begin{exercise}\n");
 printf("      {ID-fca31b79a8ac0f0be42f97014e617c1eb4900a08}\n");
 printf("      {Farbe}\n");
 printf("  \\ifproblem\\problem\n");
-printf("    Ein Zimmer ist \\simeter{5} lang und \\simeter{4} breit. Was kostet das Anstreichen\n");
-printf("    der Decke, wenn die Farbe, die man für \\simm{1} benötigt, \\eur{14} kostet?\n");
+printf("    Ein Zimmer ist \\simeter{%s} lang und \\simeter{%s} breit. Was kostet das Anstreichen\n", ins_laenge, ins_breite);
+printf("    der Decke, wenn die Farbe, die man für \\simm{1} benötigt, \\eur{%s} kostet?\n", ins_qmpreis);
 printf("  \\fi\n");
 printf("  %%\\ifoutline\\outline\n");
 printf("  %%\\fi\n");
-printf("  %%\\ifoutcome\\outcome\n");
-printf("  %%\\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Die Farbe zum Anstreichen der gesamten Zimmerdecke kostet \\eur{%s}.\n", ins_kosten);
+printf("  \\fi\n");
 printf("\\end{exercise}\n");
 
