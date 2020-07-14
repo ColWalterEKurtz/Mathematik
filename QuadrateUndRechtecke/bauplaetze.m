24e6da1e7c074f0e825e26bbec813c0718d4744f
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-07-14 15:29:17 +0200

    ergebnis berechnet: gesamtgroesse, bauplaetze

--- QuadrateUndRechtecke/bauplaetze.m
+++ QuadrateUndRechtecke/bauplaetze.m
@@ -1,17 +1,32 @@
 clear all;
 
+addpath 'Octave/';
+
+laenge   = 235;
+breite   = 185;
+infra    = 9875;
+bauplatz = 840;
+anzahl   = (laenge * breite - infra) / bauplatz;
+
+ins_laenge = sprintf("%d", laenge);
+ins_breite = sprintf("%d", breite);
+ins_infra = sprintf("%d", infra);
+ins_bauplatz = sprintf("%d", bauplatz);
+ins_anzahl = sprintf("%d", anzahl);
+
 printf("\\begin{exercise}\n");
 printf("      {ID-91d74e184ff1c45857a99e7cfd27d0b0ada040c5}\n");
 printf("      {Bauplätze}\n");
 printf("  \\ifproblem\\problem\n");
-printf("    Eine rechteckige Wiese, die \\simeter{235} lang und \\simeter{185} breit ist, soll\n");
+printf("    Eine rechteckige Wiese, die \\simeter{%s} lang und \\simeter{%s} breit ist, soll\n", ins_laenge, ins_breite);
 printf("    als Baugelände erschlossen werden. Auf Wege, Gräben und dergleichen entfallen\n");
-printf("    \\simm{9875}. Die einzelnen Bauplätze haben eine Größe von \\simm{840}.\n");
+printf("    \\simm{%s}. Die einzelnen Bauplätze haben eine Größe von jeweils \\simm{%s}.\n", ins_infra, ins_bauplatz);
 printf("    Wie viele Bauplätze erhält man?\n");
 printf("  \\fi\n");
 printf("  %%\\ifoutline\\outline\n");
 printf("  %%\\fi\n");
-printf("  %%\\ifoutcome\\outcome\n");
-printf("  %%\\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Auf diesem Gelände können insgesamt \\num{%s} Bauplätze ausgewiesen werden.\n", ins_anzahl);
+printf("  \\fi\n");
 printf("\\end{exercise}\n");
 
--- QuadrateUndRechtecke/gesamtgroesse.m
+++ QuadrateUndRechtecke/gesamtgroesse.m
@@ -1,15 +1,28 @@
 clear all;
 
+addpath 'Octave/';
+
+laenge1 = 18;
+laenge2 = 23;
+breite2 = 19;
+gesamtgroesse = laenge1 * laenge1 + laenge2 * breite2;
+
+ins_laenge1 = sprintf("%d", laenge1);
+ins_laenge2 = sprintf("%d", laenge2);
+ins_breite2 = sprintf("%d", breite2);
+ins_gesamtgroesse = sprintf("%d", gesamtgroesse);
+
 printf("\\begin{exercise}\n");
 printf("      {ID-e00899890775661f4baf8d16c87fcc09abc48395}\n");
 printf("      {Gesamtgröße}\n");
 printf("  \\ifproblem\\problem\n");
-printf("    Ein Garten ist \\simeter{18} lang und ebenso breit; ein anderer \\simeter{23} lang\n");
-printf("    und \\simeter{19} breit. Wie groß sind beide Gärten zusammen?\n");
+printf("    Ein Garten ist \\simeter{%s} lang und ebenso breit; ein anderer \\simeter{%s} lang\n", ins_laenge1, ins_laenge2);
+printf("    und \\simeter{%s} breit. Wie groß sind beide Gärten zusammen?\n", ins_breite2);
 printf("  \\fi\n");
 printf("  %%\\ifoutline\\outline\n");
 printf("  %%\\fi\n");
-printf("  %%\\ifoutcome\\outcome\n");
-printf("  %%\\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Beide Gärten sind zusammen \\simm{%s} groß.\n", ins_gesamtgroesse);
+printf("  \\fi\n");
 printf("\\end{exercise}\n");
 
