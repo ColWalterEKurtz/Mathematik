b7e734b22e9aaac0d7f0b31aeff6112be1aed2b0
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-07-14 18:41:43 +0200

    ergebnis berechnet: absperrband

--- QuadrateUndRechtecke/absperrband.m
+++ QuadrateUndRechtecke/absperrband.m
@@ -1,16 +1,30 @@
 clear all;
 
+addpath 'Octave/';
+
+laenge  = 15;
+breite  = 8;
+abstand = 2;
+band    = 2 * (laenge + 2 * abstand) + 2 * (breite + 2 * abstand);
+
+ins_laenge  = sprintf("%d", laenge);
+ins_breite  = sprintf("%d", breite);
+ins_abstand = sprintf("%d", abstand);
+ins_band    = sprintf("%d", band);
+
 printf("\\begin{exercise}\n");
 printf("      {ID-593c7b82d074b6d6c8944ec01e45f107162e029f}\n");
 printf("      {Absperrband}\n");
 printf("  \\ifproblem\\problem\n");
-printf("    Ein Bauherr soll seine Baugrube von \\simeter{15} Länge und \\simeter{8} Breite\n");
+printf("    Ein Bauherr soll seine Baugrube von \\simeter{%s} Länge und \\simeter{%s} Breite\n", ins_laenge, ins_breite);
 printf("    durch ein Band absichern. Wie viel Meter Band braucht er, wenn er es im\n");
-printf("    Abstand von \\simeter{2} zur Grube anbringen will?\n");
+printf("    Abstand von \\simeter{%s} zur Grube anbringen will?\n", ins_abstand);
 printf("  \\fi\n");
 printf("  %%\\ifoutline\\outline\n");
 printf("  %%\\fi\n");
-printf("  %%\\ifoutcome\\outcome\n");
-printf("  %%\\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Wenn das Absperrband in einem Abstand von \\simeter{%s} zur Baugrube angebracht\n", ins_abstand);
+printf("    werden soll, dann muss es \\simeter{%s} lang sein.\n", ins_band);
+printf("  \\fi\n");
 printf("\\end{exercise}\n");
 
