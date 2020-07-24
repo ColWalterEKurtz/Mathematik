f94d5bc90e06d5313f6b321549fc74e69c8f3cf4
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-07-24 12:54:26 +0200

    ergebnis berechnet: umfang

--- Pythagoras/umfang.m
+++ Pythagoras/umfang.m
@@ -1,15 +1,37 @@
 clear all;
 
+addpath 'Octave/';
+
+Ax = -2;
+Ay = -1.5;
+Bx =  3;
+By = -1;
+Cx =  0;
+Cy =  3;
+a = sqrt((Bx - Cx)^2 + (By - Cy)^2);
+b = sqrt((Ax - Cx)^2 + (Ay - Cy)^2);
+c = sqrt((Ax - Bx)^2 + (Ay - By)^2);
+u = a + b + c;
+
+ins_Ax = sprintf("%6.3f", Ax);
+ins_Ay = sprintf("%6.3f", Ay);
+ins_Bx = sprintf("%6.3f", Bx);
+ins_By = sprintf("%6.3f", By);
+ins_Cx = sprintf("%6.3f", Cx);
+ins_Cy = sprintf("%6.3f", Cy);
+ins_u  = sprintf("%.2f", u);
+
 printf("\\begin{exercise}\n");
 printf("      {ID-3946975f809c60c035f03db88b8a720b4cf813e1}\n");
 printf("      {Umfang}\n");
 printf("  \\ifproblem\\problem\n");
-printf("    Welchen Umfang hat das Dreieck?\n");
+printf("    Welchen Umfang hat das Dreieck, wenn eine Längeneinheit\n");
+printf("    im Koordinatensystem \\sicm{1} entspricht?\n");
 printf("    \\begin{center}\n");
 printf("      \\begin{tikzpicture}[scale=1]\n");
-printf("        \\coordinate (A) at (-2, -1.5);\n");
-printf("        \\coordinate (B) at ( 3, -1);\n");
-printf("        \\coordinate (C) at ( 0,  3);\n");
+printf("        \\coordinate (A) at (%s, %s);\n", ins_Ax, ins_Ay);
+printf("        \\coordinate (B) at (%s, %s);\n", ins_Bx, ins_By);
+printf("        \\coordinate (C) at (%s, %s);\n", ins_Cx, ins_Cy);
 printf("        \\draw[thin, draw=black!66!white] (-4.25, -2.25) grid[xstep=5mm, ystep=5mm] (4.25, 4.25);\n");
 printf("        \\draw[line width=1.0pt, ->, >=latex] (-4, 0) -- (4, 0) node[below]{{\\small$x$}};\n");
 printf("        \\draw[line width=1.0pt, ->, >=latex] (0, -2) -- (0, 4) node[left]{{\\small$y$}};\n");
@@ -28,7 +50,10 @@ printf("  \\fi\n");
 printf("  \\ifoutline\\outline\n");
 printf("    Benutze die Koordinaten der Eckpunkte\\ldots\n");
 printf("  \\fi\n");
-printf("  %%\\ifoutcome\\outcome\n");
-printf("  %%\\fi\n");
+printf("  \\ifoutcome\\outcome\n");
+printf("    Das Dreieck besitzt einen Umfang von ca. \\sicm{%s}.\n", ins_u);
+printf("  \\fi\n");
 printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
 
