b940e32b2c146bf20c2f409427422447813c8bc6
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2020-11-16 23:16:27 +0100

    neue aufgaben: steckbriefaufgabe 7 und 8

---
+++ Steckbriefaufgaben/steckbriefaufgabe-7.m
@@ -0,0 +1,37 @@
+clear all;
+
+addpath 'Octave/';
+
+%%% NO CALCULATIONS YET %%%
+
+printf("\\begin{exercise}\n");
+printf("      {ID-8ee49cab1227b79a1ba754aa37bd5c5424b7417b}\n");
+printf("      {Steckbriefaufgabe 7}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Ein durch den Koordinatenursprung verlaufender Graph einer ganzrationalen\n");
+printf("    Funktion dritten Grades besitzt bei $x_1=\\num{6}$ eine Nullstelle und bei\n");
+printf("    $x_2=\\num{3}$ eine Wendestelle. Die Wendetangente besitzt dort eine\n");
+printf("    Steigung von\\num{-3}. Ermitteln Sie den Funktionsterm.\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%  \\begingroup\n");
+printf("  %%    \\dimen1=5cm%%\n");
+printf("  %%    \\begin{minipage}{\\dimen1}%%\n");
+printf("  %%      \\begin{tikzpicture}%%\n");
+printf("  %%      \\end{tikzpicture}%%\n");
+printf("  %%    \\end{minipage}%%\n");
+printf("  %%    \\dimen2=\\linewidth%%\n");
+printf("  %%    \\advance\\dimen2 by -\\dimen1%%\n");
+printf("  %%    \\begin{minipage}{\\dimen2}%%\n");
+printf("  %%      \\setlength{\\abovedisplayskip}{0pt}%%\n");
+printf("  %%      \\begin{equation*}\n");
+printf("  %%      \\end{equation*}\n");
+printf("  %%    \\end{minipage}%%\n");
+printf("  %%  \\endgroup\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
---
+++ Steckbriefaufgaben/steckbriefaufgabe-8.m
@@ -0,0 +1,38 @@
+clear all;
+
+addpath 'Octave/';
+
+%%% NO CALCULATIONS YET %%%
+
+printf("\\begin{exercise}\n");
+printf("      {ID-4a9c7d3edff0ac0b12f78019308f30f5a37f280f}\n");
+printf("      {Steckbriefaufgabe 8}\n");
+printf("  \\ifproblem\\problem\n");
+printf("    Eine ganzrationale Funktion vierten Grades, deren Graph\n");
+printf("    achsensymmetrisch zur $y$-Achse verläuft, hat bei\n");
+printf("    $x_1=\\num{2}$ eine Nullstelle. Der Graph verläuft durch\n");
+printf("    den Punkt $P(\\num{1}\\mid\\num{-6})$ und hat dort eine\n");
+printf("    Steigung von \\num{-2}. Ermitteln Sie den Funktionsterm.\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\n");
+printf("  %%  \\begingroup\n");
+printf("  %%    \\dimen1=5cm%%\n");
+printf("  %%    \\begin{minipage}{\\dimen1}%%\n");
+printf("  %%      \\begin{tikzpicture}%%\n");
+printf("  %%      \\end{tikzpicture}%%\n");
+printf("  %%    \\end{minipage}%%\n");
+printf("  %%    \\dimen2=\\linewidth%%\n");
+printf("  %%    \\advance\\dimen2 by -\\dimen1%%\n");
+printf("  %%    \\begin{minipage}{\\dimen2}%%\n");
+printf("  %%      \\setlength{\\abovedisplayskip}{0pt}%%\n");
+printf("  %%      \\begin{equation*}\n");
+printf("  %%      \\end{equation*}\n");
+printf("  %%    \\end{minipage}%%\n");
+printf("  %%  \\endgroup\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+rmpath 'Octave/';
+
