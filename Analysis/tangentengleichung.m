076461c8d7192d0179ad7eefc3c90d03cc780353
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2021-06-24 21:36:21 +0200

    neue aufgabe: tangentengleichung

---
+++ Analysis/tangentengleichung.m
@@ -0,0 +1,51 @@
+clear all;
+
+%%% addpath 'Octave/';
+
+%%% NO CALCULATIONS YET %%%
+
+printf("\\begin{exercise}\n");
+printf("      {ID-f99dceda24ce3cb1bbfca5d37576481c695e6395}\n");
+printf("      {Tangentengleichung}\n");
+printf("  \\ifproblem\\problem\\par\n");
+printf("    %% <PROBLEM>\n");
+printf("    Bestimmen Sie die Funktionsgleichung der\n");
+printf("    Tangente $t$ an den Graphen von $f$ in\n");
+printf("    Punkt $P\\left(x_0\\;\\middle|\\;f(x_0)\\right)$.\n");
+printf("    Geben Sie anschließend auch den Schnittpunkt\n");
+printf("    der Tangente mit der $x$-Achse an.\n");
+printf("    \\begin{alignat*}{3}\n");
+printf("      \\text{a)}&\\;\\;\n");
+printf("      f(x)=-x^2\n");
+printf("      \\quad\n");
+printf("      P\\left(2\\;\\middle|\\;f(2)\\right)\n");
+printf("      \\qquad&\\qquad\n");
+printf("      \\text{c)}&\\;\\;\n");
+printf("      f(x)=-x^2+3\n");
+printf("      \\quad\n");
+printf("      P\\left(4\\;\\middle|\\;f(4)\\right)\n");
+printf("      \\\\[1ex]\n");
+printf("      \\text{b)}&\\;\\;\n");
+printf("      f(x)=2x^2-x\n");
+printf("      \\quad\n");
+printf("      P\\left(-3\\;\\middle|\\;f(-3)\\right)\n");
+printf("      \\qquad&\\qquad\n");
+printf("      \\text{d)}&\\;\\;\n");
+printf("      f(x)=-5x+3\n");
+printf("      \\quad\n");
+printf("      P\\left(0\\;\\middle|\\;f(0)\\right)\n");
+printf("    \\end{alignat*}\n");
+printf("    %% </PROBLEM>\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\\par\n");
+printf("    %% <OUTLINE>\n");
+printf("    %% </OUTLINE>\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\\par\n");
+printf("    %% <OUTCOME>\n");
+printf("    %% </OUTCOME>\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+%%% rmpath 'Octave/';
+
