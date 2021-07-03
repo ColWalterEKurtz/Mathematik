92befe944e190bc55cade6ab44269b7486a7d7ee
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2021-07-04 01:29:03 +0200

    aufgabenstellung erweitert: pkw

--- Kreisgeometrie/pkw.m
+++ Kreisgeometrie/pkw.m
@@ -1,33 +1,34 @@
 clear all;
 
-addpath 'Octave/';
+%%% addpath 'Octave/';
 
-n = 400;                         % [1]
-s = 1;                           % [km]
-d = (s * 1000 * 100) / (n * pi); % [cm}
-
-ins_n = sprintf("%d",   n);
-ins_s = sprintf("%d",   s);
-ins_d = sprintf("%.1f", d);
+%%% NO CALCULATIONS YET %%%
 
 printf("\\begin{exercise}\n");
-printf("      {ID-d426995888a6d1e22f96b3a3d0ef30ead32b8b05}\n");
+printf("      {ID-3c830994e3dd4f59586c8fae5bf3c81f9f49059c}\n");
 printf("      {Pkw}\n");
 printf("  \\ifproblem\\problem\\par\n");
-printf("    Das Rad eines Pkw muss sich \\num{%s} Mal drehen, damit es \\sikm{%s}\n", ins_n, ins_s);
-printf("    zurücklegt. Welchen Durchmesser hat es?\n");
+printf("    %% <PROBLEM>\n");
+printf("    Das Rad eines Pkws muss sich \\num{400} Mal drehen,\n");
+printf("    damit es \\SI{1}{\\kilo\\metre} zurücklegt.\n");
+printf("    \\begin{enumerate}[a)]\n");
+printf("      \\item Welchen Durchmesser hat das Rad?\n");
+printf("      \\item Mit wie vielen Umdrehungen pro Minute\n");
+printf("            dreht sich das Rad, wenn der Pkw\n");
+printf("            \\SI{90}{\\kilo\\metre\\per\\hour}\n");
+printf("            schnell fährt?\n");
+printf("    \\end{enumerate}\n");
+printf("    %% </PROBLEM>\n");
 printf("  \\fi\n");
-printf("  \\ifoutline\\outline\\par\n");
-printf("    \\begin{equation*}\n");
-printf("      U=2\\pi r\n");
-printf("      \\quad\\Rightarrow\\quad\n");
-printf("      2r=d=\\frac{U}{\\pi}\n");
-printf("    \\end{equation*}\n");
-printf("  \\fi\n");
-printf("  \\ifoutcome\\outcome\\par\n");
-printf("    Das Rad hat einen Durchmesser von ca. \\sicm{%s}.\n", ins_d);
-printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\\par\n");
+printf("    %% <OUTLINE>\n");
+printf("    %% </OUTLINE>\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\\par\n");
+printf("    %% <OUTCOME>\n");
+printf("    %% </OUTCOME>\n");
+printf("  %%\\fi\n");
 printf("\\end{exercise}\n");
 
-rmpath 'Octave/';
+%%% rmpath 'Octave/';
 
