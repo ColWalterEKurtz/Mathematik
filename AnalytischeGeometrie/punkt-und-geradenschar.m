47b94e81ee30243a6f51abc14c2282b46bd61ff4
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2021-07-04 02:01:39 +0200

    neue aufgaben: drohne; punkt und geradenschar

---
+++ AnalytischeGeometrie/punkt-und-geradenschar.m
@@ -0,0 +1,40 @@
+clear all;
+
+%%% addpath 'Octave/';
+
+%%% NO CALCULATIONS YET %%%
+
+printf("\\begin{exercise}\n");
+printf("      {ID-2ebf4539419bf07c1de89f8e1b7a981800f091dd}\n");
+printf("      {Punkt und Geradenschar}\n");
+printf("  \\ifproblem\\problem\\par\n");
+printf("    %% <PROBLEM>\n");
+printf("    Durch folgende Definitionen seien ein Punkt $A$ und eine Geradenschar $g_u$ gegeben:\n");
+printf("    \\begin{equation*}\n");
+printf("      A\\left(1\\;\\middle|\\;2\\;\\middle|\\;3\\right)\n");
+printf("      \\qquad\n");
+printf("      \\qquad\n");
+printf("      g_u:\\,\\vec x=\\vec p+r\\cdot\\vec v=\\begin{pmatrix}-2\\\\1\\\\2\\end{pmatrix}+r\\cdot\\begin{pmatrix}1\\\\u\\\\1-u\\end{pmatrix}\n");
+printf("    \\end{equation*}\n");
+printf("    \\begin{enumerate}[a)]\n");
+printf("      %%\\setlength{\\itemsep}{-1ex}%%\n");
+printf("      \\item Wie weit ist $A$ von $g_0$ entfernt?\n");
+printf("      \\item Gibt es einen Wert für $u$, so dass der Punkt $A$ auf der\n");
+printf("            Geraden $g_u$ liegt?\n");
+printf("      \\item Welche Gerade $g_u$ kommt $A$ am nächsten?\n");
+printf("      \\item Kann der Abstand zwischen $A$ und einer Geraden beliebig groß werden?\n");
+printf("    \\end{enumerate}\n");
+printf("    %% </PROBLEM>\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\\par\n");
+printf("    %% <OUTLINE>\n");
+printf("    %% </OUTLINE>\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\\par\n");
+printf("    %% <OUTCOME>\n");
+printf("    %% </OUTCOME>\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+%%% rmpath 'Octave/';
+
