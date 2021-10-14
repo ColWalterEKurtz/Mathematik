f4f29157712ead64dd9468ee9b7695a3fba22e1d
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2021-10-14 02:46:20 +0200

    neue aufgabe hinzugefuegt: punkt und gerade

---
+++ Extremwertprobleme/punkt-und-gerade.m
@@ -0,0 +1,29 @@
+clear all;
+
+%%% addpath 'Octave/';
+
+%%% NO CALCULATIONS YET %%%
+
+printf("\\begin{exercise}\n");
+printf("      {ID-fdbba083a62a98df2574e5c4bf9c7b049f7eb89b}\n");
+printf("      {Punkt und Gerade}\n");
+printf("  \\ifproblem\\problem\\par\n");
+printf("    %% <PROBLEM>\n");
+printf("    Welcher Punkt auf der Geraden $g$ mit der\n");
+printf("    Funktionsgleichung $g(x)=x+1$ hat vom Punkt\n");
+printf("    $P\\left(3\\;\\middle|\\;-1\\right)$ minimalen\n");
+printf("    Abstand?\n");
+printf("    %% </PROBLEM>\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\\par\n");
+printf("    %% <OUTLINE>\n");
+printf("    %% </OUTLINE>\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\\par\n");
+printf("    %% <OUTCOME>\n");
+printf("    %% </OUTCOME>\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+%%% rmpath 'Octave/';
+
