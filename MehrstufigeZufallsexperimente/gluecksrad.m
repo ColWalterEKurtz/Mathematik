5cb279ce25f005a1dfa86b9570b16b8b25e286a6
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2021-10-15 18:32:13 +0200

    neue aufgabe hinzugefuegt: gluecksrad

---
+++ MehrstufigeZufallsexperimente/gluecksrad.m
@@ -0,0 +1,64 @@
+clear all;
+
+%%% addpath 'Octave/';
+
+%%% NO CALCULATIONS YET %%%
+
+printf("\\begin{exercise}\n");
+printf("      {ID-13ad0ca691a242e47d568c79816250e9c8f26cda}\n");
+printf("      {Glücksrad}\n");
+printf("  \\ifproblem\\problem\\par\n");
+printf("    %% <PROBLEM>\n");
+printf("    Für ein Spiel wird ein Glücksrad verwendet, das drei farbigen Sektoren\n");
+printf("    besitzt. Der Tabelle können die Farben der Sektoren und die Größen der\n");
+printf("    zugehörigen Mittelpunktswinkel entnommen werden.\n");
+printf("    \\begin{center}\n");
+printf("      \\renewcommand{\\arraystretch}{1.25}%%\n");
+printf("      \\begin{tabular}{|l|c|c|c|}\n");
+printf("        \\hline\n");
+printf("        Farbe\n");
+printf("        & \\makebox[4em][c]{Blau}\n");
+printf("        & \\makebox[4em][c]{Rot}\n");
+printf("        & \\makebox[4em][c]{Grün}\n");
+printf("        \\\\\n");
+printf("        \\hline\n");
+printf("        Mittelpunktswinkel\n");
+printf("        & $180^\\circ$\n");
+printf("        & $120^\\circ$\n");
+printf("        & $60^\\circ$\n");
+printf("        \\\\\n");
+printf("        \\hline\n");
+printf("      \\end{tabular}\n");
+printf("    \\end{center}\n");
+printf("    Für einen Einsatz von 5 Euro darf ein Spieler das Glücksrad dreimal\n");
+printf("    drehen. Erzielt der Spieler dreimal die gleiche Farbe, werden ihm 10\n");
+printf("    Euro ausgezahlt. Erzielt er drei verschiedene Farben, wird ein anderer\n");
+printf("    Betrag ausgezahlt. In allen anderen Fällen erfolgt keine Auszahlung.\n");
+printf("    \\begin{enumerate}[a)]\n");
+printf("      \\item Bestimme die Wahrscheinlichkeit der folgenden Ereignisse:\n");
+printf("            \\begin{equation*}\n");
+printf("              \\begin{split}\n");
+printf("                E_1:\\;\\;&\\text{\\glqq drei gleiche Farben\\grqq}\n");
+printf("                \\\\\n");
+printf("                E_2:\\;\\;&\\text{\\glqq drei verschiedene Farben\\grqq}\n");
+printf("              \\end{split}\n");
+printf("            \\end{equation*}\n");
+printf("      \\item Bei dem Spiel ist zu erwarten, dass sich die Einsätze der Spieler\n");
+printf("            und die Auszahlungen auf lange Sicht ausgleichen. Bestimme den\n");
+printf("            Betrag, der ausgezahlt wird, wenn drei verschiedene Farben\n");
+printf("            erscheinen.\n");
+printf("    \\end{enumerate}\n");
+printf("    %% </PROBLEM>\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\\par\n");
+printf("    %% <OUTLINE>\n");
+printf("    %% </OUTLINE>\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\\par\n");
+printf("    %% <OUTCOME>\n");
+printf("    %% </OUTCOME>\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+%%% rmpath 'Octave/';
+
