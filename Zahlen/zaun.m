2c4d53c03643854cc6c24bde09ab8da61cb3f081
Col. Walter E. Kurtz <Col.Walter.E.Kurtz@gmail.com>
2021-10-19 03:27:20 +0200

    neue aufgabe hinzugefuegt: zaun

---
+++ Zahlen/zaun.m
@@ -0,0 +1,64 @@
+clear all;
+
+addpath 'Octave/';
+
+% --
+% sd
+% --
+%
+% string decimal
+%
+% x       number to convert
+% opt(1)  maxdcp
+% opt(2)  addbrn
+% opt(3)  addbrf
+% opt(4)  addsgn
+% opt(5)  addtex
+%
+function str = sd(x, opt = [7 0 0 0 1])
+  str = myn2s(x, opt(1), opt(2), opt(3), opt(4), opt(5));
+endfunction
+
+% --
+% sf
+% --
+%
+% string fraction
+%
+% x       number to convert
+% opt(1)  maxdcp
+% opt(2)  addbrn
+% opt(3)  addbrf
+% opt(4)  addsgn
+% opt(5)  addtex
+%
+function str = sf(x, opt = [0 0 0 0 1])
+  str = myn2s(x, opt(1), opt(2), opt(3), opt(4), opt(5));
+endfunction
+
+%%% NO CALCULATIONS YET %%%
+
+printf("\\begin{exercise}\n");
+printf("      {ID-f6984cf366c7211209d720bfe345f0549391d1d3}\n");
+printf("      {Zaun}\n");
+printf("  \\ifproblem\\problem\\par\n");
+printf("    %% <PROBLEM>\n");
+printf("    Ein rechteckiges Grundstück soll eingezäunt werden.\n");
+printf("    Es ist \\SI{120}{\\metre} lang und \\SI{84}{\\metre}\n");
+printf("    breit. Der Abstand von einem Zaunpfahl zum Nächsten\n");
+printf("    soll überall gleich groß sein.\n");
+printf("    Wie viele Zaunpfähle braucht man mindestens?\n");
+printf("    %% </PROBLEM>\n");
+printf("  \\fi\n");
+printf("  %%\\ifoutline\\outline\\par\n");
+printf("    %% <OUTLINE>\n");
+printf("    %% </OUTLINE>\n");
+printf("  %%\\fi\n");
+printf("  %%\\ifoutcome\\outcome\\par\n");
+printf("    %% <OUTCOME>\n");
+printf("    %% </OUTCOME>\n");
+printf("  %%\\fi\n");
+printf("\\end{exercise}\n");
+
+%%% rmpath 'Octave/';
+
